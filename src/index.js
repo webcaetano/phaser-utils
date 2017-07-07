143593ae4f4bf94c1d7bb76c57e5cdcbfaa79355
Andre Caetano <webandrecaetano@gmail.com>
2017-07-07 09:48:36 -0300

    essentials build

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ var Phaser = require('phaser');
 
 var self = {}
 
-self.ang = {
-	to:{
-		rad:function(a){
-			return a*(180/Math.PI)
-		},
-		deg:function(a){
-			return a*(Math.PI/180);
-		}
-	}
-}
+// self.ang = {
+// 	to:{
+// 		rad:function(a){
+// 			return a*(180/Math.PI)
+// 		},
+// 		deg:function(a){
+// 			return a*(Math.PI/180);
+// 		}
+// 	}
+// }
 
 self.randInRange = function(point,range){
 	var a = (Math.random()*360) * (Math.PI / 180);
@@ -22,100 +22,100 @@ self.randInRange = function(point,range){
 	}
 }
 
-self.elipsePos = function(point,angle,size){
-	var a = angle * (Math.PI / 180);
-	return {
-		x: point.x + Math.cos(a) * size.width/2,
-		y: point.y + Math.sin(a) * size.height/2
-	};
-}
+// self.elipsePos = function(point,angle,size){
+// 	var a = angle * (Math.PI / 180);
+// 	return {
+// 		x: point.x + Math.cos(a) * size.width/2,
+// 		y: point.y + Math.sin(a) * size.height/2
+// 	};
+// }
 
-self.mod = function(n,a){
-	return ((n % a) + a) % a;
-}
+// self.mod = function(n,a){
+// 	return ((n % a) + a) % a;
+// }
 
-self.getPorInRange = function (min, max, current) {
-	if (current >= max) return 1;
-	if (current <= min) return 0;
+// self.getPorInRange = function (min, max, current) {
+// 	if (current >= max) return 1;
+// 	if (current <= min) return 0;
 
-	return (current - min) / (max - min);
-};
+// 	return (current - min) / (max - min);
+// };
 
-self.pointAngleIntersection = function(point1,angle1,point2,angle2) {
-	// get any point on the line1
-	var line1 = {
-		start:point1,
-		end:self.radPos(point1,angle1,50),
-	}
+// self.pointAngleIntersection = function(point1,angle1,point2,angle2) {
+// 	// get any point on the line1
+// 	var line1 = {
+// 		start:point1,
+// 		end:self.radPos(point1,angle1,50),
+// 	}
 
-	// get any point on the line2
-	var line2 = {
-		start:point2,
-		end:self.radPos(point2,angle2,50)
-	}
+// 	// get any point on the line2
+// 	var line2 = {
+// 		start:point2,
+// 		end:self.radPos(point2,angle2,50)
+// 	}
 
-	var denominator = (((line2.end.y - line2.start.y) * (line1.end.x - line1.start.x)) - ((line2.end.x - line2.start.x) * (line1.end.y - line1.start.y)));
+// 	var denominator = (((line2.end.y - line2.start.y) * (line1.end.x - line1.start.x)) - ((line2.end.x - line2.start.x) * (line1.end.y - line1.start.y)));
 
-	// if is parallel return null;
-	if(denominator===0) return null;
-	var a = (((line2.end.x - line2.start.x) * (line1.start.y - line2.start.y)) - ((line2.end.y - line2.start.y) * (line1.start.x - line2.start.x))) / denominator;
+// 	// if is parallel return null;
+// 	if(denominator===0) return null;
+// 	var a = (((line2.end.x - line2.start.x) * (line1.start.y - line2.start.y)) - ((line2.end.y - line2.start.y) * (line1.start.x - line2.start.x))) / denominator;
 
-	return {
-		x:line1.start.x + (a * (line1.end.x - line1.start.x)),
-		y:line1.start.y + (a * (line1.end.y - line1.start.y))
-	};
-}
+// 	return {
+// 		x:line1.start.x + (a * (line1.end.x - line1.start.x)),
+// 		y:line1.start.y + (a * (line1.end.y - line1.start.y))
+// 	};
+// }
 
-self.getCenter = function(obj){
-	return {
-		x:obj.x+(obj.width*0.5),
-		y:obj.y+(obj.height*0.5),
-	}
-}
+// self.getCenter = function(obj){
+// 	return {
+// 		x:obj.x+(obj.width*0.5),
+// 		y:obj.y+(obj.height*0.5),
+// 	}
+// }
 
-self.time = function(){
-	return Math.floor(+new Date()/1000);
-}
+// self.time = function(){
+// 	return Math.floor(+new Date()/1000);
+// }
 
-self.porBetween = function(min,max,curent,por,fixed){
-	if(por===undefined) por = 100;
-	if(fixed===undefined) fixed = 2;
+// self.porBetween = function(min,max,curent,por,fixed){
+// 	if(por===undefined) por = 100;
+// 	if(fixed===undefined) fixed = 2;
 
-	return Number(((curent-min)*por/(max-min)).toFixed(fixed))
-}
+// 	return Number(((curent-min)*por/(max-min)).toFixed(fixed))
+// }
 
-self.hitDotBox = function(dot,box){
-	return dot.x>box.x &&
-	dot.x<box.x+box.width &&
-	dot.y>box.y &&
-	dot.y<box.y+box.height;
-}
+// self.hitDotBox = function(dot,box){
+// 	return dot.x>box.x &&
+// 	dot.x<box.x+box.width &&
+// 	dot.y>box.y &&
+// 	dot.y<box.y+box.height;
+// }
 
-self.hitBoxBox = function(box1,box2){
-	return box1.x+box1.width>box2.x &&
-	box1.x<box2.x+box2.width &&
-	box1.y+box1.height>box2.y &&
-	box1.y<box2.y+box2.height;
-}
+// self.hitBoxBox = function(box1,box2){
+// 	return box1.x+box1.width>box2.x &&
+// 	box1.x<box2.x+box2.width &&
+// 	box1.y+box1.height>box2.y &&
+// 	box1.y<box2.y+box2.height;
+// }
 
-self.dotsBetween = function(point1,point2,amount){
-	if(amount===undefined) amount = 1;
+// self.dotsBetween = function(point1,point2,amount){
+// 	if(amount===undefined) amount = 1;
 
-	var resp = [];
-	var dist = self.dist(point1,point2);
-	var a = self.angleBetween(point1,point2);
-	return Array.apply(null, {length: 10}).map((val,i)=> i).map(function(i){
-		var currentPos = (dist/(amount+1))*(i+1);
-		return {
-			x:point1.x + Math.cos(a)*currentPos,
-			y:point1.y + Math.sin(a)*currentPos
-		}
-	});
-}
+// 	var resp = [];
+// 	var dist = self.dist(point1,point2);
+// 	var a = self.angleBetween(point1,point2);
+// 	return Array.apply(null, {length: 10}).map((val,i)=> i).map(function(i){
+// 		var currentPos = (dist/(amount+1))*(i+1);
+// 		return {
+// 			x:point1.x + Math.cos(a)*currentPos,
+// 			y:point1.y + Math.sin(a)*currentPos
+// 		}
+// 	});
+// }
 
-self.dotLine = function(point1,point2,amount){
-	return [point1].concat(self.dotsBetween(point1,point2,amount-2)).concat([point2]);
-}
+// self.dotLine = function(point1,point2,amount){
+// 	return [point1].concat(self.dotsBetween(point1,point2,amount-2)).concat([point2]);
+// }
 
 self.angleBetween = function(point1,point2){
 	return Math.atan2(point2.y-point1.y, point2.x-point1.x);
@@ -145,41 +145,40 @@ self.radPos = function(point,angle,range){
 	}
 }
 
-self.getRangeIndexByValue = function(length,current,modes,ASC){
-	if(ASC===undefined) ASC=true;
+// self.getRangeIndexByValue = function(length,current,modes,ASC){
+// 	if(ASC===undefined) ASC=true;
 
-	if(current>=length) current=length-1;
-	if(current<=0) current=0;
+// 	if(current>=length) current=length-1;
+// 	if(current<=0) current=0;
 
-	return ~~(modes*(current/length));
-}
+// 	return ~~(modes*(current/length));
+// }
 
-self.getModeByValue = function(length,current,modes,ASC){
-	if(ASC===undefined) ASC=true;
-	return modes[self.getRangeIndexByValue(length,current,modes.length,ASC)];
-}
+// self.getModeByValue = function(length,current,modes,ASC){
+// 	if(ASC===undefined) ASC=true;
+// 	return modes[self.getRangeIndexByValue(length,current,modes.length,ASC)];
+// }
 
-self.randPorRange = function(val,por){
-	var part = Math.ceil(val*por/100);
-	return self.rand(val-part,val+part);
-}
+// self.randPorRange = function(val,por){
+// 	var part = Math.ceil(val*por/100);
+// 	return self.rand(val-part,val+part);
+// }
 
-self.loadAssets = function(game,assets){
-	var i;
-	for(i in assets.atlas) {
-		game.load.atlasJSONHash(i, assets.atlas[i].image, assets.atlas[i].jsonUrl, assets.atlas[i].json);
-	}
-	for(i in assets.images) game.load.image(i, assets.images[i]);
-	for(i in assets.sprites) game.load.spritesheet(i, assets.sprites[i].image, assets.sprites[i].width, assets.sprites[i].height, assets.sprites[i].frames);
-	for(i in assets.audio) game.load.audio(i, assets.audio[i]);
-}
+// self.loadAssets = function(game,assets){
+// 	for(let i in assets.atlas) {
+// 		game.load.atlasJSONHash(i, assets.atlas[i].image, assets.atlas[i].jsonUrl, assets.atlas[i].json);
+// 	}
+// 	for(let i in assets.images) game.load.image(i, assets.images[i]);
+// 	for(let i in assets.sprites) game.load.spritesheet(i, assets.sprites[i].image, assets.sprites[i].width, assets.sprites[i].height, assets.sprites[i].frames);
+// 	for(let i in assets.audio) game.load.audio(i, assets.audio[i]);
+// }
 
-self.por = function(val,por,plus,fix){
-	if(plus===undefined) plus=true;
-	if(fix===undefined) fix=0;
+// self.por = function(val,por,plus,fix){
+// 	if(plus===undefined) plus=true;
+// 	if(fix===undefined) fix=0;
 
-	return Number(((val * por / 100)*(plus ? 1 : -1)+val).toFixed(fix));
-}
+// 	return Number(((val * por / 100)*(plus ? 1 : -1)+val).toFixed(fix));
+// }
 
 self.dist = function(obj1,obj2){
 	return Math.sqrt(Math.pow(obj1.x-obj2.x,2)+Math.pow(obj1.y-obj2.y,2));
@@ -219,25 +218,25 @@ self.setBtnHold = function(obj,callback,callback2){
 	return obj;
 }
 
-self.setHover = function(obj,callback,callback2){
-	if(callback===undefined) callback=null;
-	if(callback2===undefined) callback2=null;
+// self.setHover = function(obj,callback,callback2){
+// 	if(callback===undefined) callback=null;
+// 	if(callback2===undefined) callback2=null;
 
-	if(!obj) return;
-	if(!obj.inputEnabled) obj.inputEnabled = true;
-	if(!obj.input) return;
-	obj.input.useHandCursor = true;
-	if(callback){
-		obj.events.onInputOver.add(function(e){
-			callback(e);
-		});
-	}
-	if(callback2){
-		obj.events.onInputOut.add(function(e){
-			callback2(e);
-		});
-	}
-	return obj;
-}
+// 	if(!obj) return;
+// 	if(!obj.inputEnabled) obj.inputEnabled = true;
+// 	if(!obj.input) return;
+// 	obj.input.useHandCursor = true;
+// 	if(callback){
+// 		obj.events.onInputOver.add(function(e){
+// 			callback(e);
+// 		});
+// 	}
+// 	if(callback2){
+// 		obj.events.onInputOut.add(function(e){
+// 			callback2(e);
+// 		});
+// 	}
+// 	return obj;
+// }
 
 module.exports = self;
